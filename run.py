90bf870aa81e68611e3b4c80a1d4fd608a78fbe5
Alva Lind <alvalinds97@gmail.com>
2024-02-11 16:27:10 +0000

    Create function to hide the picked word. Pick a random word from gamewords

--- run.py
+++ run.py
@@ -4,3 +4,16 @@ print("......................................................")
 
 gameWords = ["australia", "sweden", "finland", "norway", "denmark", "poland", "ireland", "spain", "italy", "portugal", "greece", "albania", "ukrain", "switzerland", "cyprus", "argentina", "mauritius", "canada", "united states", "mexico", "morocco", "brazil", "chile", "philippines", "new zealand", "qatar", "estonia", "france", "singapore", "germany", "guatemala", "turkey", "jordan", "syria", "japan", "china", "russia"]
 
+def display_hidden_word(pickWord):
+    """
+    Creates an underscore for every letter and a space for the spaces between words where required.
+    """
+    for x in pickWord:
+        if x == ' ':
+            print(" ", end=" ")
+        else:
+            print("_", end=" ")
+
+pickWord = random.choice(gameWords)
+pickWord = pickWord.lower() # convert to lowercase to make case-insensitive
+display_hidden_word(pickWord)
