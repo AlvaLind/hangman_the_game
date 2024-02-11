6bb2ec241acdc7e9a98a904d0721c5c63adb5ca9
Alva Lind <alvalinds97@gmail.com>
2024-02-11 16:15:51 +0000

    Add welcome message and gamewords list

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print("Welcome! The theme of this game is countries. Let's play hangman!")
+print("......................................................")
+
+gameWords = ["australia", "sweden", "finland", "norway", "denmark", "poland", "ireland", "spain", "italy", "portugal", "greece", "albania", "ukrain", "switzerland", "cyprus", "argentina", "mauritius", "canada", "united states", "mexico", "morocco", "brazil", "chile", "philippines", "new zealand", "qatar", "estonia", "france", "singapore", "germany", "guatemala", "turkey", "jordan", "syria", "japan", "china", "russia"]
+
