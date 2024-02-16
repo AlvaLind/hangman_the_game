3b1cfb967f99cf2668a5badd33c1ffd4238f5642
Alva Lind <alvalinds97@gmail.com>
2024-02-16 16:48:16 +0000

    Instructions function file

---
+++ instructions.py
@@ -0,0 +1,14 @@
+def instructions():
+    """
+    Prints the game Instructions 
+    """
+    print("--------------------")
+    print("Hangman Instructions")
+    print("--------------------")
+    print("Guess the letters to reveal the hidden country before the man gets hanged.")
+    print("You can guess as many times as you want but you only have 7 incorrect guesses")
+    print("until you loose. So use your guesses carefully!")
+    print("Each guess must only be a single letter.")
+    print("Good luck! Begin or continue your game below.")
+    print("--------------------")
+    
