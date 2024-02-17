16ae93df16e24d4f05cf9e4ef5a434be1a77fa6d
Alva Lind <alvalinds97@gmail.com>
2024-02-17 11:27:31 +0000

    Move countries theme choices to seperate file

---
+++ countries.py
@@ -0,0 +1,13 @@
+import random
+
+def get_countries_words():
+    theme_words = [
+        "australia", "sweden", "finland", "norway", "denmark",
+        "poland", "ireland", "spain", "italy", "madagascar", "fiji",
+        "portugal", "greece", "albania", "ukraine", "switzerland",
+        "argentina", "canada", "united states", "mexico",
+        "morocco", "brazil", "chile", "philippines", "new zealand", "qatar",
+        "estonia", "france", "singapore", "germany", "guatemala", "turkey",
+        "jordan", "syria", "japan", "china", "russia", "egypt", "south africa"
+    ]
+    return theme_words
