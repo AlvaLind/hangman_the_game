6d0c15667fdc68aed867e090421f05822990f4a4
Alva Lind <alvalinds97@gmail.com>
2024-02-17 11:26:03 +0000

    Move sports theme to separate file and add more sports

---
+++ sports.py
@@ -0,0 +1,15 @@
+import random
+
+def get_sports_words():
+    theme_words = [
+        "soccer", "basketball", "tennis", "baseball", "volleyball",
+        "golf", "cricket", "swimming", "boxing", "cycling",
+        "athletics", "rugby", "hockey", "skiing", "surfing",
+        "wrestling", "karate", "rowing", "fencing", "gymnastics",
+        "table tennis", "badminton", "ice skating", "rock climbing",
+        "sailing", "canoeing", "kayaking", "archery", "triathlon",
+        "mountain biking", "horse racing", "weightlifting", "judo",
+        "taekwondo", "handball", "water polo", "figure skating", "bobsleigh",
+        "luge", "synchronized swimming", "trampoline", "pentathlon", "biathlon"
+    ]
+    return theme_words
