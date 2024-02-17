717bfb0026ed1aab02d55d6c7c3abea05e31a7ab
Alva Lind <alvalinds97@gmail.com>
2024-02-17 11:33:08 +0000

    Move cities theme choices to seperate file and add more cities to the list

---
+++ cities.py
@@ -0,0 +1,18 @@
+import random
+
+def get_cities_words():
+    theme_words = [
+        "new york", "london", "paris", "tokyo", "los angeles",
+        "berlin", "mumbai", "beijing", "moscow", "cairo",
+        "istanbul", "shanghai", "delhi", "rio de janeiro", "mexico city",
+        "sydney", "rome", "toronto", "bangkok", "dubai",
+        "chicago", "singapore", "seoul", "amsterdam", "hong kong",
+        "jakarta", "barcelona", "buenos aires", "miami", "athens", "oslo",
+        "helsinki", "san francisco", "cape town", "budapest", "prague",
+        "vienna", "lisbon", "manila", "kuala lumpur", "stockholm", "zurich",
+        "doha", "san paulo", "dallas", "phoenix", "jakarta", "saint petersburg"
+        "boston", "manchester", "edinburgh", "glasgow", "vancouver", "auckland",
+        "wellington", "christchurch", "melbourne", "brisbane", "perth",
+        "adelaide", "copenhagen"
+    ]
+    return theme_words
